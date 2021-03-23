11d2adec69323b1d79a9175a9cc7dc8662813ca2
capyyimg <79929439+capyyimg@users.noreply.github.com>
2021-03-23 14:11:14 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,40 @@
+let Walk_button = 0
+input.onButtonPressed(Button.A, function () {
+    Walk_button += 1
+})
 basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P0, 0)
     pins.digitalWritePin(DigitalPin.P2, 1)
-    basic.pause(2000)
-    for (let index = 0; index < 4; index++) {
+    basic.pause(5000)
+    for (let index = 0; index < 3; index++) {
         pins.digitalWritePin(DigitalPin.P2, 0)
-        basic.pause(100)
+        basic.pause(150)
         pins.digitalWritePin(DigitalPin.P2, 1)
-    }
-    basic.pause(500)
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    basic.pause(2000)
-    for (let index = 0; index < 4; index++) {
-        pins.digitalWritePin(DigitalPin.P0, 0)
-        basic.pause(100)
-        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(150)
     }
     pins.digitalWritePin(DigitalPin.P2, 0)
     pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.pause(500)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.pause(5000)
+    for (let index = 0; index < 3; index++) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        basic.pause(150)
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(150)
+    }
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    Walk_button = 0
+})
+basic.forever(function () {
+    while (Walk_button == 1 && pins.digitalReadPin(DigitalPin.P0) == 1) {
+        music.playTone(831, music.beat(BeatFraction.Half))
+    }
+    while (Walk_button == 1 && pins.digitalReadPin(DigitalPin.P2) == 1) {
+        music.playTone(698, music.beat(BeatFraction.Half))
+        music.playTone(587, music.beat(BeatFraction.Half))
+        music.playTone(523, music.beat(BeatFraction.Half))
+        music.playTone(466, music.beat(BeatFraction.Half))
+        music.playTone(466, music.beat(BeatFraction.Quarter))
+    }
 })
