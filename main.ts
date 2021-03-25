983acea4a47a06091e9238473f31508f96155110
capyyimg <79929439+capyyimg@users.noreply.github.com>
2021-03-24 21:49:03 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,20 +8,25 @@ let Walk_button = 0
 let malfunction = 0
 basic.forever(function () {
     if (malfunction == 0) {
-        pins.digitalWritePin(DigitalPin.P2, 1)
-        basic.pause(5000)
+        for (let green_light = 0; green_light <= 5; green_light++) {
+            pins.digitalWritePin(DigitalPin.P2, 1)
+            basic.pause(1000)
+        }
         for (let index = 0; index < 3; index++) {
             pins.digitalWritePin(DigitalPin.P2, 0)
             basic.pause(150)
             pins.digitalWritePin(DigitalPin.P2, 1)
             basic.pause(150)
         }
+        Walk_button = 0
         pins.digitalWritePin(DigitalPin.P2, 0)
         pins.digitalWritePin(DigitalPin.P1, 1)
-        basic.pause(500)
+        basic.pause(2500)
         pins.digitalWritePin(DigitalPin.P1, 0)
-        pins.digitalWritePin(DigitalPin.P0, 1)
-        basic.pause(5000)
+        for (let red_light = 0; red_light <= 5; red_light++) {
+            pins.digitalWritePin(DigitalPin.P0, 1)
+            basic.pause(1000)
+        }
         for (let index = 0; index < 3; index++) {
             pins.digitalWritePin(DigitalPin.P0, 0)
             basic.pause(150)
@@ -29,13 +34,32 @@ basic.forever(function () {
             basic.pause(150)
         }
         pins.digitalWritePin(DigitalPin.P0, 0)
-        Walk_button = 0
+        for (let turns = 0; turns <= 8; turns++) {
+            basic.showLeds(`
+                . . # . .
+                . # . . .
+                # # # # #
+                . # . . .
+                . . # . .
+                `)
+            basic.pause(100)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+            basic.pause(100)
+        }
     } else if (malfunction == 1) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
         basic.pause(200)
         pins.digitalWritePin(DigitalPin.P1, 1)
         basic.pause(200)
         pins.digitalWritePin(DigitalPin.P1, 0)
     } else if (malfunction == 2) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
         basic.pause(200)
         pins.digitalWritePin(DigitalPin.P0, 1)
         basic.pause(200)
@@ -45,6 +69,7 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
+    let green_light = 0
     if (Walk_button >= 1 && pins.digitalReadPin(DigitalPin.P0) == 1) {
         basic.showLeds(`
             # # # # #
@@ -63,7 +88,7 @@ basic.forever(function () {
             # # # # #
             `)
         basic.clearScreen()
-    } else if (Walk_button >= 1 && 0 == 0) {
+    } else if (Walk_button >= 1 && green_light <= 3) {
         basic.showLeds(`
             . . # . .
             . # # # .
@@ -72,7 +97,6 @@ basic.forever(function () {
             . # . # .
             `)
         basic.pause(100)
-    } else {
-    	
+        basic.clearScreen()
     }
 })
